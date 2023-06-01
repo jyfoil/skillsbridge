4b8127149dfcdde7063fc247fcafba5002ee7bfb
Scott Coons <dscoons@gmail.com>
2023-06-01 15:51:07 -0400

    Home Vue file updated - fully styled with login working

--- final_capstone-main/capstone/vue/src/components/HeaderNav.vue
+++ final_capstone-main/capstone/vue/src/components/HeaderNav.vue
@@ -3,11 +3,15 @@
         <router-link :to="{ name: 'home' }" class="header-link">
             <div class="logo-container"><img src="../assets/elearning.png" alt="Logo" class="logo"></div>
         </router-link>
-        <h4 class="portal-title">{{ msg }}</h4>
-        <div id="nav">
+        <h3 class="portal-title">{{ msg }}</h3>
+        <div id="nav" v-if="$store.state.token != ''">
             <a href="#">My Courses</a>
             <div id="notifications" class="icon alert show"><img src="../assets/notifications.svg" /><div id="notification-tray"><ul><li>Notification #1</li><li>Notification #2</li></ul></div></div>
-            <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+            <router-link v-bind:to="{ name: 'logout' }" >Logout</router-link>
+        </div>
+        <div id="nav" v-else>
+            <router-link v-bind:to="{ name: 'login' }" >Login</router-link>
+            <router-link v-bind:to="{ name: 'register' }" >Register</router-link>
         </div>
     </header>
 </template>
@@ -18,8 +22,11 @@ export default {
 }
 </script>
 
-<style>
-    header { display:flex; align-items:center; padding-bottom:10px; }
+<style scoped>
+    header { display:flex; align-items:center; padding: 10px; }
+    header .portal-title { flex-grow:1; }
     #notification-tray { display: none; position:absolute; }
-    #notifications.show #notification-tray { display:block; right: 0; min-width: 200px; border: 1px solid #CCC; box-shadow: 0 0 10px rgba(0,0,0,0.1); background:white; }
+    #notifications.show #notification-tray { display:block; right: 0; min-width: 200px; border: 1px solid #CCC; box-shadow: 0 0 10px rgba(0,0,0,0.1); background:white; z-index:100; }
+    header a { color:#429CB9; text-decoration:none; font-weight:bold; }
+    header a:hover { color:#195468; }
 </style>
