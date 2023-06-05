f1a172379fb69c85d9706a44a1111c6357e29146
Kelsey Bouma <boumakelsey@gmail.com>
2023-06-05 16:26:07 -0400

    work on student dashboard

--- final_capstone-main/capstone/vue/src/views/StudentHome.vue
+++ final_capstone-main/capstone/vue/src/views/StudentHome.vue
@@ -1,27 +1,52 @@
 <template>
-    <div>
-        <div class="dashboard">
-            <h1 class="dashboard-title">{{ userFirstName }}'s Dashboard</h1>
+    <div class="dashboard">
+         <div id="heading-bg">
+            <h1 class="dashboard-title">{{ $store.state.user.fullname }} - Student Dashboard</h1>
         </div>
+        <main id="dashboard-content">
+            <div id="content">
+                <h2>My Courses</h2>
+                <section id="courses">
+                   <course-listing :course="course" v-for="course in courses" :key="course.courseId" />
+                </section>
+            </div>
+            <section>
+                <h3>Upcoming Assignments</h3>
+            </section>
+        </main>
     </div>
+   
 </template>
 
 
 
 <script>
-
+import courseService from '../services/CourseService.js'
+import CourseListing from '../components/CourseListing.vue'
 export default {
-    components: {
-        
+    data() {
+        return {
+             courses: [],
+        }
     },
-  computed: {
-      userFirstName() {
-          const username = this.$store.state.user.username;
-
-          const firstName = username
-
-          return firstName;
-      },
-  },
+    computed: {
+        userFirstName() {
+            const username = this.$store.state.user.username;
+            const firstName = username
+            return firstName;
+        },
+    },
+    created: function() {
+        this.courses = courseService.getCoursesForUser().then(response => {
+            if (response.status === 200) {
+                this.courses = response.data;
+            }
+        });
+        //console.log(this.courses);
+    },
+    components: {
+        CourseListing,
+    },
+    
 };
 </script>
