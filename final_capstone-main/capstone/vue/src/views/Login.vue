0a8f843abff38dba329c7072c38d898f3dd6da80
Kelsey Bouma <boumakelsey@gmail.com>
2023-06-05 11:50:21 -0400

    header finally working

--- final_capstone-main/capstone/vue/src/views/Login.vue
+++ final_capstone-main/capstone/vue/src/views/Login.vue
@@ -16,7 +16,7 @@
         v-if="this.$route.query.registration"
       >Thank you for registering, please sign in.</div>
       <div>
-      <label for="username" class="sr-only">Username</label>
+      <!-- <label for="username" class="sr-only">Username</label> -->
       <input
         type="text"
         id="username"
@@ -27,7 +27,7 @@
         autofocus
       /></div>
       <div>
-      <label for="password" class="sr-only">Password</label>
+      <!-- <label for="password" class="sr-only">Password</label> -->
       <input
         type="password"
         id="password"
